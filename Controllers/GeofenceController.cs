c0bf4364f5be409248842b53adfa4d27985cb4e9
DonNacho-ok <56751437+DonNacho-ok@users.noreply.github.com>
2020-02-11 10:50:28 -0300

    Add files via upload

---
+++ Controllers/GeofenceController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.WebTracking.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace api.WebTracking.Controllers
+{
+    [Controller]
+    [Route("[Controller]")]
+    public class GeofenceController : Controller
+    {
+        //Get
+        [HttpGet]
+        public IList<Geofence> Get()
+        {
+            var geolist = new trackingContext().Geofence.ToList();
+            return geolist;
+        }
+        //Post
+        [HttpPost]
+        public void Post([FromBody]Geofence Geofence)
+        {
+            var context = new trackingContext();
+            var geo = new Geofence
+            {
+                Id = Geofence.Id,
+                Name = Geofence.Name,
+                Description = Geofence.Description,
+                Latitude=Geofence.Latitude,
+                Longitude=Geofence.Longitude,
+                Radius=Geofence.Radius,
+                Enterpriseid=Geofence.Enterpriseid,
+                Geofencetypeid=Geofence.Geofencetypeid,
+                Active=Geofence.Active
+            };
+            context.Add<Geofence>(geo);
+            context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void Put([FromBody]Geofence updatedGeofence)
+        {
+            var context = new trackingContext();
+
+            var geo = context.Geofence.Where(g => g.Id == updatedGeofence.Id).SingleOrDefault();
+            geo.Name = updatedGeofence.Name;
+            geo.Description = updatedGeofence.Description;
+            geo.Latitude = updatedGeofence.Latitude;
+            geo.Longitude = updatedGeofence.Longitude;
+            geo.Radius = updatedGeofence.Radius;
+            geo.Enterpriseid = updatedGeofence.Enterpriseid;
+            geo.Geofencetypeid = updatedGeofence.Geofencetypeid;
+            geo.Active = updatedGeofence.Active;
+
+            context.Update<Geofence>(geo);
+            context.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody] Geofence deletedGeofence)
+        {
+            var context = new trackingContext();
+
+            var geo = context.Geofence.Where(g => g.Id == deletedGeofence.Id).SingleOrDefault();
+
+            context.Remove<Geofence>(geo);
+            context.SaveChanges();
+        }
+
+    }
+}
---
+++ Controllers/StateController.cs
@@ -0,0 +1,58 @@
+﻿using api.WebTracking.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.WebTracking.Controllers
+{
+    [Controller]
+    [Route("[Controller]")]
+    public class StateController : ControllerBase
+    {
+        [HttpGet]
+        public IList<State> Get()
+        {
+            return new trackingContext().State.ToList();
+        }
+
+        [HttpPost]
+        public void Post([FromBody]State State)
+        {
+            var context = new trackingContext();
+            var NewState = new State
+            {
+                Id = State.Id,
+                Name = State.Name,
+                Countryid = State.Countryid,
+            };
+            context.Add<State>(NewState);
+            context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void Put([FromBody]State updatedState)
+        {
+            var context = new trackingContext();
+            var state = context.State.Where(s => s.Id == updatedState.Id).SingleOrDefault();
+            
+            state.Id = updatedState.Id;
+            state.Name = updatedState.Name;
+            state.Countryid = updatedState.Countryid;
+
+            context.Update<State>(state);
+            context.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody]State deletedState)
+        {
+            var context = new trackingContext();
+            var state = context.State.Where(s => s.Id == deletedState.Id).SingleOrDefault();
+
+            context.Remove<State>(state);
+            context.SaveChanges();
+        }
+    }
+}
